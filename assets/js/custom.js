ec6f20599939dc857f6c011f97a2d2efcc702bf8
ozzaynetworks <ozzaynetworks@gmail.com>
2022-02-21 18:48:50 +0300

    kelvin: member details added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,6 +24,8 @@ $(window).on('load', function() {
 
 
     $('.selectpicker').selectpicker();
+
+    //changing the payment method
     $('[name*="payment"]').on('change', function() {
         var paymentOption = $(this).val();
         if (paymentOption == "Mpesa-payment") {
@@ -36,6 +38,9 @@ $(window).on('load', function() {
         }
     })
 
-    // changing the payment method
+    // OTP stepper
+    function moveToNext(t, u) { 0 < t.value.length && $("#digit" + u + "-input").focus() }
+    var count = 1;
+    $(".two-step").keyup(function(t) { 0 == count && (count = 1), 8 === t.keyCode ? (5 == count && (count = 3), $("#digit" + count + "-input").focus(), count--) : 0 < count && (count++, $("#digit" + count + "-input").focus()) });
 
 });
