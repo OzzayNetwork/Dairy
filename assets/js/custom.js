7817750d5ed52625d8190df26524a61f3b386ac6
ozzaynetworks <ozzaynetworks@gmail.com>
2022-02-24 04:44:49 +0300

    kelvin: member deductions added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,7 +23,6 @@ $(window).on('load', function() {
     });
 
 
-    $('.selectpicker').selectpicker();
 
     //changing the payment method
     $('[name*="payment"]').on('change', function() {
@@ -42,5 +41,13 @@ $(window).on('load', function() {
     function moveToNext(t, u) { 0 < t.value.length && $("#digit" + u + "-input").focus() }
     var count = 1;
     $(".two-step").keyup(function(t) { 0 == count && (count = 1), 8 === t.keyCode ? (5 == count && (count = 3), $("#digit" + count + "-input").focus(), count--) : 0 < count && (count++, $("#digit" + count + "-input").focus()) });
+    $('body').on("click", '#datatable-buttons_wrapper td .dropdown>a',
+        function() {
+            $(this).parent().parent().parent().addClass("highlighted-raw").siblings().removeClass("highlighted-raw");
+        });
+
+
+    $('.selectpicker').selectpicker();
+
 
 });
