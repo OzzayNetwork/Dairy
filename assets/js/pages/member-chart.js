fed26c39151f07e0f97db45d6bf470554b6e7a25
ozzaynetworks <ozzaynetworks@gmail.com>
2022-02-24 03:04:21 +0300

    kelvin: the members initial pages

--- assets/js/pages/member-chart.js
+++ assets/js/pages/member-chart.js
@@ -1,31 +1,160 @@
-var options = { series: [{ name: "BTC", data: [12, 14, 2, 47, 42, 15, 47, 75, 65, 19, 14] }], chart: { type: "area", height: 40, sparkline: { enabled: !0 } }, stroke: { curve: "smooth", width: 2 }, colors: ["#f1b44c"], fill: { type: "gradient", gradient: { shadeIntensity: 1, inverseColors: !1, opacityFrom: .45, opacityTo: .05, stops: [25, 100, 100, 100] } }, tooltip: { fixed: { enabled: !1 }, x: { show: !1 }, marker: { show: !1 } } },
+//net milk collection line graph
+var options = {
+        series: [{ name: "Net Collections", data: [12, 14, 2, 47, 42, 15, 47, 75, 65, 19, 14, 32] }],
+        xaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        },
+        chart: { type: "area", height: 40, sparkline: { enabled: !0 } },
+        stroke: { curve: "smooth", width: 2 },
+        colors: ["#34c38f"],
+        fill: { type: "gradient", gradient: { shadeIntensity: 1, inverseColors: !1, opacityFrom: .45, opacityTo: .05, stops: [25, 100, 100, 100] } },
+        tooltip: { fixed: { enabled: !1 }, x: { show: true }, marker: { show: !1 } },
+        tooltip: {
+            y: {
+                formatter: function(value, x, { series, seriesIndex, dataPointIndex, w }) {
+                    return numeral(value).format('0,0')
+
+                }
+            }
+        },
+
+    },
     chart = new ApexCharts(document.querySelector("#area-sparkline-chart-1"), options);
 chart.render();
 
+// rejected quantity graph
+
 options = {
-    series: [{ name: "ETH", data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54] }],
+    series: [{ name: "Rejected", data: [25, 66, 41, 89, 63, 25, 44, 12, 36, 9, 54] }],
     chart: { type: "area", height: 40, sparkline: { enabled: !0 } },
     stroke: { curve: "smooth", width: 2 },
-    colors: ["#3452e1"],
+    colors: ["#f46a6a"],
     fill: { type: "gradient", gradient: { shadeIntensity: 1, inverseColors: !1, opacityFrom: .45, opacityTo: .05, stops: [25, 100, 100, 100] } },
     tooltip: { fixed: { enabled: !1 }, x: { show: !1 }, marker: { show: !1 } }
 };
 (chart = new ApexCharts(document.querySelector("#area-sparkline-chart-2"), options)).render();
 
+// total cash deductions
+
 options = {
-    series: [{ name: "LTC", data: [35, 53, 93, 47, 54, 24, 47, 75, 65, 19, 14] }],
+    series: [{ name: "Deductions", data: [35, 53, 93, 47, 54, 24, 47, 75, 65, 19, 14] }],
     chart: { type: "area", height: 40, sparkline: { enabled: !0 } },
     stroke: { curve: "smooth", width: 2 },
-    colors: ["#50a5f1"],
+    colors: ["#f1b44c"],
     fill: { type: "gradient", gradient: { shadeIntensity: 1, inverseColors: !1, opacityFrom: .45, opacityTo: .05, stops: [25, 100, 100, 100] } },
-    tooltip: { fixed: { enabled: !1 }, x: { show: !1 }, marker: { show: !1 } }
+    tooltip: { fixed: { enabled: !1 }, x: { show: !1 }, marker: { show: !1 } },
+    tooltip: {
+        x: { format: "dd MMM yyyy" },
+        y: {
+            formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+                return numeral(value).format('0,0.00') + " KG"
+
+            }
+        }
+    },
 };
 (chart = new ApexCharts(document.querySelector("#area-sparkline-chart-3"), options)).render();
-var walletOptions = { series: [76, 67, 61], chart: { height: 302, type: "radialBar" }, plotOptions: { radialBar: { offsetY: 0, startAngle: 0, endAngle: 270, hollow: { margin: 5, size: "35%", background: "transparent", image: void 0 }, track: { show: !0, startAngle: void 0, endAngle: void 0, background: "#f2f2f2", strokeWidth: "97%", opacity: 1, margin: 12, dropShadow: { enabled: !1, top: 0, left: 0, blur: 3, opacity: .5 } }, dataLabels: { name: { show: !0, fontSize: "16px", fontWeight: 600, offsetY: -10 }, value: { show: !0, fontSize: "14px", offsetY: 4, formatter: function(e) { return e + "%" } }, total: { show: !0, label: "Total", color: "#373d3f", fontSize: "16px", fontFamily: void 0, fontWeight: 600, formatter: function(e) { return e.globals.seriesTotals.reduce(function(e, t) { return e + t }, 0) + "%" } } } } }, stroke: { lineCap: "round" }, colors: ["#3452e1", "#f1b44c", "#50a5f1"], labels: ["Ethereum", "Bitcoin", "Litecoin"], legend: { show: !1 } };
-(chart = new ApexCharts(document.querySelector("#wallet-balance-chart"), walletOptions)).render();
+
+
+// farmer payments graph
+var options = {
+        chart: {
+            height: 360,
+            type: "bar",
+            stacked: !1,
+            toolbar: {
+                show: !1
+            },
+            zoom: {
+                enabled: !0
+            },
+
+        },
+        plotOptions: {
+            bar: {
+                horizontal: !1,
+                columnWidth: "40%",
+                // endingShape: "rounded"
+            }
+        },
+        dataLabels: {
+            enabled: !1,
+        },
+        stroke: { show: !0, width: 2, colors: ["transparent"] },
+
+        yaxis: {
+            labels: {
+                formatter: function(value) {
+                    // return "KES " + value;
+                    return numeral(value).format('0,0 a')
+                },
+                // formatter: function(val, index) {
+
+                //     return numeral(val).format('0,0')
+                // },
+
+
+
+            },
+            title: {
+                text: "Amount in KES",
+            }
+        },
+        series: [{
+            name: "Net Wedges",
+            data: [4023658, 5123456, 41458975, 67123654, 22123654, 43789654, 36789623, 52320365, 24023147, 18012586, 36036985, 48025820]
+        }, {
+            name: "Deductions",
+            data: [13025856, 23025632, 20032145, 8021457, 13032568, 27456987, 18235897, 22235789, 10213214, 16365478, 24456987, 22123568]
+        }],
+        xaxis: {
+            categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        },
+        colors: ["#556ee6", "#f46a6a"],
+        legend: {
+            position: "bottom"
+        },
+        fill: {
+            opacity: 1
+        },
+
+        tooltip: {
+            custom: function({ series, seriesIndex, dataPointIndex, w }) {
+                var data = w.globals.initialSeries[seriesIndex].data[dataPointIndex];
+
+                return '<ul>' +
+                    '<li><b>Price</b>: ' + data.x + '</li>' +
+                    '<li><b>Number</b>: ' + data.y + '</li>' +
+                    '<li><b>Product</b>: \'' + data.product + '\'</li>' +
+                    '<li><b>Info</b>: \'' + data.info + '\'</li>' +
+                    '<li><b>Site</b>: \'' + data.site + '\'</li>' +
+                    '</ul>';
+            }
+        },
+
+        tooltip: {
+            y: {
+                formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+                    return "KES " + numeral(value).format('0,0')
+
+                }
+            }
+        },
+        tooltip: {
+            y: [{ title: { formatter: function(e) { return e + " (mins)" } } },
+                { title: { formatter: function(e) { return e + " per session" } } },
+                { title: { formatter: function(e) { return e } } }
+            ]
+        },
+
+    },
+    chart = new ApexCharts(document.querySelector("#member-salary-chart"), options);
+chart.render();
+
+// milk collections main chart
 options = {
     series: [{
-        name: "BTC",
+        name: "Collected Milk",
         data: [
             [13273596e5, 30.95],
             [1327446e6, 31.34],
@@ -304,18 +433,39 @@ options = {
             [13619196e5, 39.6]
         ]
     }],
-    chart: { type: "area", height: 240, toolbar: "false" },
+    chart: { type: "area", height: 350, toolbar: "false" },
+    yaxis: {
+        labels: {
+            formatter: function(value) {
+                // return "KES " + value;
+                return numeral(value).format('0,0 a')
+            },
+            // formatter: function(val, index) {
+
+            //     return numeral(val).format('0,0')
+            // },
+
+
+
+        },
+        title: {
+            text: "Collected milk In KGs",
+        }
+    },
     dataLabels: { enabled: !1 },
     stroke: { curve: "smooth", width: 2 },
     markers: { size: 0, style: "hollow" },
     xaxis: { type: "datetime", min: new Date("01 Mar 2012").getTime(), tickAmount: 6 },
-    tooltip: { x: { format: "dd MMM yyyy" } },
+    tooltip: {
+        x: { format: "dd MMM yyyy" },
+        y: {
+            formatter: function(value, { series, seriesIndex, dataPointIndex, w }) {
+                return numeral(value).format('0,0.00') + " KG"
+
+            }
+        }
+    },
     colors: ["#f1b44c"],
     fill: { type: "gradient", gradient: { shadeIntensity: 1, opacityFrom: .6, opacityTo: .05, stops: [42, 100, 100, 100] } }
 };
 (chart = new ApexCharts(document.querySelector("#overview-chart-timeline"), options)).render();
-var resetCssClasses = function(e) {
-    var t = document.querySelectorAll("button");
-    Array.prototype.forEach.call(t, function(e) { e.classList.remove("active") }), e.target.classList.add("active")
-};
-document.querySelector("#one_month").addEventListener("click", function(e) { resetCssClasses(e), chart.updateOptions({ xaxis: { min: new Date("28 Jan 2013").getTime(), max: new Date("27 Feb 2013").getTime() } }) }), document.querySelector("#six_months").addEventListener("click", function(e) { resetCssClasses(e), chart.updateOptions({ xaxis: { min: new Date("27 Sep 2012").getTime(), max: new Date("27 Feb 2013").getTime() } }) }), document.querySelector("#one_year").addEventListener("click", function(e) { resetCssClasses(e), chart.updateOptions({ xaxis: { min: new Date("27 Feb 2012").getTime(), max: new Date("27 Feb 2013").getTime() } }) }), document.querySelector("#all").addEventListener("click", function(e) { resetCssClasses(e), chart.updateOptions({ xaxis: { min: void 0, max: void 0 } }) });
