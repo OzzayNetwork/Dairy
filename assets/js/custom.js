0bf32a9ea8f59967423c5705437dc6be45500621
Kelvin Njuguna <ozzaynetwork@gmail.com>
2024-05-28 11:22:21 +0300

    kelvin: added AI option pages

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,6 +23,161 @@ $(window).on('load', function() {
     });
 
 
+    $(document).ready(function() {
+
+        $('.dismin-modal').on('click', function(){
+            $('#payment-modal').modal('hide')
+        })
+    
+        $('.print-bill-btn').on('click', function(){
+            // alert("clicked")
+            $('.payment-next').click();
+            $('.bill-modal-footer').removeClass('d-none')
+        })
+       
+        //sending payment
+        $('body').on('click','.payment-next', function(){
+            // alert( $(".payment-panel-parent .payment-panel.payment-active-panel").index())
+    
+            $('.payment-prev').prop('disabled', false)       
+            var countTheSteps = parseFloat($(".payment-panel-parent .payment-panel").length);
+            var theCurrentIndex= $(".payment-panel-parent .payment-panel.payment-active-panel").index();
+            theCurrentIndex=theCurrentIndex+1;
+            if(theCurrentIndex!=countTheSteps){
+                $('.payment-panel-parent').find('.payment-active-panel').addClass('d-none').removeClass('payment-active-panel').next().removeClass('d-none').addClass('payment-active-panel')
+            }
+            if (theCurrentIndex==countTheSteps-1) {
+                $('.payment-next').addClass('d-none');
+            }
+    
+            // else{
+            //     $('.payment-next').addClass('d-none'); 
+            // }
+              
+            
+    
+        })
+    
+        $('body').on('click', '.btn-next', function(){
+            $(this).siblings('.btn-prev').prop('disabled', false)
+            
+            var theStepsCont=$(this).parent().parent().siblings('.steps-container')
+            var numberOfChildren=theStepsCont.children().length
+            var activeStedIndex=theStepsCont.children('.the-step.active').index()
+            var activeStep=theStepsCont.children('.the-step.active')
+            var nextStep=theStepsCont.children('.the-step.active').index()+1
+            if(nextStep!=numberOfChildren){
+                activeStep.addClass('d-none').removeClass('active').next().removeClass('d-none').addClass('active')
+            }
+    
+            if(nextStep===numberOfChildren-1){
+                $(this).prop('disabled', true).addClass('d-none')
+                $(this).siblings('.btn-submit').removeClass('d-none')
+            }
+    
+        })
+    
+        $('body').on('click', '.btn-prev', function(){
+    
+            $(this).siblings('.btn-next').prop('disabled', false).removeClass('d-none')
+            $(this).siblings('.btn-submit').prop('disabled', false).addClass('d-none')
+            
+            var theStepsCont=$(this).parent().parent().siblings('.steps-container')
+            var numberOfChildren=theStepsCont.children().length
+            var activeStedIndex=theStepsCont.children('.the-step.active').index()
+            var activeStep=theStepsCont.children('.the-step.active')
+            var nextStep=theStepsCont.children('.the-step.active').index()-1
+            if(nextStep>-1){
+                activeStep.addClass('d-none').removeClass('active').prev().removeClass('d-none').addClass('active')
+            }
+    
+            if(nextStep<1){
+                $(this).prop('disabled', true)
+               
+            }
+    
+        })
+    
+        $('body').on('click','.payment-prev', function(){
+            $('.payment-next').removeClass('d-none');  
+            var countTheSteps = parseFloat($(".payment-panel-parent .payment-panel").length);
+            var theCurrentIndex= $(".payment-panel-parent .payment-panel.payment-active-panel").index();
+            if(theCurrentIndex!=0){
+                $('.payment-panel-parent').find('.payment-active-panel').addClass('d-none').removeClass('payment-active-panel').prev().removeClass('d-none').addClass('payment-active-panel')
+                $('.transaction-summary').addClass('d-none')
+            }   
+            
+        });
+    
+        function containsWordOrSentence(str, wordOrSentence) {
+            return str.includes(wordOrSentence);
+          }
+    
+    
+        $('body').on('change','#accessible-platforms', function(){
+    
+           
+    
+            var theVal=$(this).val()
+            var dashboard=containsWordOrSentence(theVal,"County Biller Dashboard")
+            var enforcementApp=containsWordOrSentence(theVal,"Collections & Enforcement App")
+            var supervisor=containsWordOrSentence(theVal,"Supervision Mobile App")
+    
+            const dashboardCheckboxes = $('.dashboard-rights input[type="checkbox"]');
+            const dashboardCheckboxes2 = $('.dashboard-modules input[type="checkbox"]');
+            const enforcementCheckboxes = $('.enforcement-modules input[type="checkbox"]');
+            const supervisorCheckboxes = $('.supervisor-modules input[type="checkbox"]');
+    
+            
+            
+    
+            dashboardCheckboxes.prop('disabled', !dashboard);
+            dashboardCheckboxes2.prop('disabled', !dashboard);
+    
+            enforcementCheckboxes.prop('disabled', !enforcementApp);
+            supervisorCheckboxes.prop('disabled', !supervisor);
+    
+           
+    
+            
+            
+        })
+    
+        $('body').on('change','.check-dash', function(){
+           //$('.check-dash').siblings('label').text("Show")
+    
+           var id = $(this).attr('id');
+    
+           var isChecked = isCheckboxChecked(id);
+    
+           if (isChecked) {
+                $('.check-dash').siblings('label').text("Hide")
+                $('.check-dash').prop("checked", true);
+    
+                $('.check-dash').siblings('label').text("Show").parent().parent().parent().parent().removeClass('bg-warning').addClass('bg-white').siblings().removeClass('d-none')
+            } else {
+                $('.check-dash').siblings('label').text("Show").parent().parent().parent().parent().addClass('bg-warning').removeClass('bg-white').siblings().addClass('d-none')
+                $('.check-dash').prop("checked", false);
+            }
+        })
+    
+        function isCheckboxChecked(checkboxId) {
+            var checkbox = document.getElementById(checkboxId);
+            if (checkbox) {
+              return checkbox.checked;
+            } else {
+              console.error("Checkbox with ID " + checkboxId + " not found.");
+              return false; // Assuming unchecked if the checkbox is not found
+            }
+          }
+          
+    
+    
+        $('.selectpicker').selectpicker();
+        $('.selectpicker').selectpicker('render')
+    });
+
+
 
     //changing the payment method
     $('[name*="payment"]').on('change', function() {
@@ -58,3 +213,4 @@ $(window).on('load', function() {
 
 
 });
+
